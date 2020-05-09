e7a71304566a3676615203347076dbd0cb0f8f4e
John Curry <fastpenguin91@gmail.com>
2020-05-09 14:53:31 -0700

    change bgColor of block

--- src/blocks/test-one/index.js
+++ src/blocks/test-one/index.js
@@ -4,6 +4,14 @@ import { registerBlockType } from "@wordpress/blocks";
 import { __ } from "@wordpress/i18n";
 import { RichText } from "@wordpress/editor";
 
+
+// const blockStyle = {
+//     backgroundColor: '#900',
+//     color: '#fff',
+//     padding: '20px',
+// };
+
+
 // This is working even though its not registered in PHP. why?
 registerBlockType('jsc-courses/test-one', {
     title: 'test one',
@@ -13,17 +21,41 @@ registerBlockType('jsc-courses/test-one', {
     attributes: {
         content: {
             type: 'string'
+        },
+        bgColor: {
+            type: 'string'
         }
     },
     edit: function ({ className, attributes, setAttributes }) {
+        let blockStyle = {
+            backgroundColor: attributes.bgColor,
+        }
         const { content } = attributes;
-        return <RichText
-            tagName="p"
-            className={className}
-            onChange={(value) => setAttributes({ content: value })}
-            value={content}
-            formattingControls={['bold']}
-        />
+        return <div style={blockStyle} className="background-wrapper">
+            <RichText
+                tagName="p"
+                className={className}
+                onChange={(value) => {
+                    setAttributes({ content: value, bgColor: "green" })
+                    console.log("it works?");
+                    console.log(attributes.bgColor);
+                }}
+                value={content}
+                formattingControls={['bold']}
+            />
+            <RichText
+                tagName="p"
+                className={className}
+                onChange={(value) => {
+                    setAttributes({ bgColor: value })
+                    console.log("it works?");
+                    console.log(value);
+                    console.log(attributes.bgColor);
+                }}
+                value={attributes.bgColor}
+                formattingControls={['bold']}
+            />
+        </div>
     },
     save: () => {
         return <div> dunno what to do</div>
