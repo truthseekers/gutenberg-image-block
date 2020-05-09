2d21ff588e5ab8b938e53d357c4d5903d4c0c46e
John Curry <fastpenguin91@gmail.com>
2020-05-09 11:07:14 -0700

    stylesheets loaded

--- jsc-courses.php
+++ jsc-courses.php
@@ -14,7 +14,7 @@
 // copied...
 
 register_block_type(
-    'jsc-courses/lesson',
+    'jsc-courses/custom-image',
     array(
         'editor_script' => 'jsc-courses-editor-script',
         'editor_style' => 'jsc-courses-editor-style',
@@ -25,19 +25,6 @@ register_block_type(
 );
 
 
-
-function jsc_courses_enqueue()
-{
-
-
-    // What is the difference between wp_enqueue_script and the other?
-    wp_enqueue_script(
-        'jsc-courses-script',
-        plugins_url('build/index.js', __FILE__)
-        //, array('wp-blocks') not necessary because using wp-scripts
-    );
-}
-
 function jsc_courses_block_register()
 {
 
@@ -46,8 +33,109 @@ function jsc_courses_block_register()
         plugins_url('build/index.js', __FILE__), // This use to be the src/editor.js but webpack.
         array('wp-blocks', 'wp-i18n', 'wp-element')
     );
+
+    wp_register_style(
+        'jsc-courses-editor-style',
+        plugins_url('build/style.css', __FILE__),
+        array('wp-edit-blocks')
+    );
 }
 
 add_action('init', 'jsc_courses_block_register');
+
+
+
+
+//////////////////////////////////
+
+function gutenberg_examples_01_register_block()
+{
+    // automatically load dependencies and version
+    $asset_file = include(plugin_dir_path(__FILE__) . 'build/index.asset.php');
+
+    wp_register_script(
+        'gutenberg-examples-01-esnext',
+        plugins_url('build/block.js', __FILE__),
+        $asset_file['dependencies'],
+        $asset_file['version']
+    );
+
+    register_block_type('gutenberg-examples/example-01-basic-esnext', array(
+        'editor_script' => 'gutenberg-examples-01-esnext',
+    ));
+
+    ////////////////////////////////
+
+
+    wp_register_style(
+        'gutenberg-examples-02-editor',
+        plugins_url('editor.css', __FILE__),
+        array('wp-edit-blocks')
+        //filemtime(plugin_dir_path(__FILE__) . 'editor.css')
+    );
+
+    wp_register_style(
+        'gutenberg-examples-02',
+        plugins_url('style.css', __FILE__),
+        array()
+        //filemtime(plugin_dir_path(__FILE__) . 'style.css')
+    );
+
+
+    register_block_type('gutenberg-examples/example-02-stylesheets', array(
+        'style' => 'gutenberg-examples-02',
+        'editor_style' => 'gutenberg-examples-02-editor',
+        'editor_script' => 'gutenberg-examples-02',
+    ));
+}
+
+
+add_action('init', 'gutenberg_examples_01_register_block');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myguten_stylesheet()
+// {
+//     wp_enqueue_style('myguten-style', plugins_url('style.css', __FILE__));
+// }
+
+// add_action('enqueue_block_assets', 'myguten_stylesheet');
+
+
+
+
+// function jsc_courses_enqueue()
+// {
+
+
+//     // What is the difference between wp_enqueue_script and the other?
+//     wp_enqueue_script(
+//         'jsc-courses-script',
+//         plugins_url('build/index.js', __FILE__)
+//         //, array('wp-blocks') not necessary because using wp-scripts
+//     );
+// }
+
+
+
 // not sure if I even need the below script
 //add_action('enqueue_block_editor_assets', 'jsc_courses_enqueue');
--- src/index.js
+++ src/index.js
@@ -1,14 +1,141 @@
 console.log("in the index.js of src in jsc-courses");
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, registerBlockStyle } from '@wordpress/blocks';
+import { __ } from "@wordpress/i18n";
+
+// import './styles.editor.scss';
+// import './style.scss';
+// import { registerBlockType } from "@wordpress/blocks";
+// import edit from './edit';
+// import { RichText } from "@wordpress/editor";
+// import { Dashicon } from "@wordpress/components";
+// const attributes = {
+//     id: {
+//         type: 'number'
+//     },
+//     alt: {
+//         type: 'string',
+//         source: 'attribute',
+//         selector: 'img',
+//         attribute: 'alt',
+//         default: ''
+//     },
+//     url: {
+//         type: 'string',
+//         source: 'attribute',
+//         selector: 'img',
+//         attribute: 'src',
+//     },
+// }
+
+// registerBlockType('mytheme-blocks/jsc-image2', {
+//     title: __('JSC IMAGE 2', 'mytheme-blocks'),
+
+//     description: __('Block showing a Team Member.', 'mytheme-blocks'),
+
+//     icon: 'admin-users',
+
+//     supports: {
+//         reusable: false,
+//         html: false
+//     },
+
+//     category: "mytheme-category",
+
+//     keywords: [__('team', 'mytheme-blocks'), __('member', 'mytheme-blocks'), __('person', 'mytheme-blocks')],
+
+//     attributes,
+
+//     save: ({ attributes }) => {
+//         const { title, info, url, alt, id, social } = attributes;
+//         console.log("img id: ", id);
+//         return (
+//             <div>
+//                 <img src={url} id={id} alt={alt} className={id ? `jsc-lightbox-img wp-image-${id}` : null} />
+//                 <div className={"outer"}>
+//                     <div className="outercenter">
+//                         <div className="outerx">
+//                             <span className="close">&times;</span>
+//                             {/* <span className="x">X</span> */}
+//                         </div>
+//                         <div className="first">
+//                             <img className={"jsc-lightbox"} src={url} alt={alt} />
+//                         </div>
+//                     </div>
+//                 </div>
+//                 {title &&
+//                     <RichText.Content
+//                         className={'wp-block-mytheme-blocks-team-member__title'}
+//                         tagName="h4"
+//                         value={title}
+//                     />
+//                 }
+//             </div>
+//         )
+//     },
+//     edit
+// });
 
 
 
 
 
-registerBlockType('jsc-courses/lesson', {
-    title: 'jsc lesson',
+
+
+registerBlockType('jsc-courses/custom-image', {
+    title: 'custom image',
+    description: __('A simple lightbox image.', 'jsc-courses'),
     icon: 'smiley',
+    keywords: [__('image', 'jsc-courses'), __('photo', 'jsc-courses'), __('picture', 'jsc-courses')],
     category: 'layout',
-    edit: () => <div>Lesson 1</div>,
-    save: () => <div>Hola, mundo!</div>,
+    edit: function ({ className }) { // destructuring from props
+        console.log("props!");
+        console.log(className);
+        return <div className={className}>Lesson 1 in a function yo</div>
+    },
+    save: () => {
+        return <div>Hola, mundo!</div>
+    }
+});
+
+registerBlockStyle('jsc-courses/custom-image', {
+    name: 'fancy-quote',
+    label: 'Fancy Quote',
+});
+
+///////////////////
+
+//import { registerBlockType } from '@wordpress/blocks';
+
+const blockStyle = {
+    backgroundColor: '#900',
+    color: '#fff',
+    padding: '20px',
+};
+
+registerBlockType('gutenberg-examples/example-01-basic-esnext', {
+    title: 'Example: Basic (esnext)',
+    icon: 'universal-access-alt',
+    category: 'layout',
+    example: {},
+    edit() {
+        return <div style={blockStyle}>Hello World, step 1million (from the editor).</div>;
+    },
+    save() {
+        return <div style={blockStyle}>Hello World, step 1 (from the frontend).</div>;
+    },
+});
+
+
+
+registerBlockType('gutenberg-examples/example-02-stylesheets', {
+    title: 'Example Stylesheets',
+    icon: 'universal-access-alt',
+    category: 'layout',
+    example: {},
+    edit({ className }) {
+        return <p className={className}>Hello World, step 2 (from the editor, in green).</p>;
+    },
+    save() {
+        return <p>Hello World, step 2 (from the frontend, in red).</p>;
+    },
 });
