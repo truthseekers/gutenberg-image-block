d7f52a765824b5a67fceb4d78de5814c527ae9a7
John Curry <fastpenguin91@gmail.com>
2020-05-24 11:06:13 -0700

    transforms complete

--- src/blocks/jsc-custom-image/index.js
+++ src/blocks/jsc-custom-image/index.js
@@ -1,6 +1,6 @@
 // import './style.editor.css';
 // import './style.css';
-import { registerBlockType, registerBlockStyle } from "@wordpress/blocks";
+import { registerBlockType, registerBlockStyle, createBlock } from "@wordpress/blocks";
 import { __ } from "@wordpress/i18n";
 import edit from "./edit";
 import { RichText } from "@wordpress/editor";
@@ -54,6 +54,32 @@ registerBlockType('jsc-courses/jsc-custom-image', {
     keywords: [__('image', 'jsc-courses'), __('photo', 'jsc-courses'), ('img', 'jsc-courses'), __('picture', 'jsc-courses')],
 
     attributes,
+
+    transforms: {
+        to: [
+            {
+                type: 'block',
+                blocks: ['core/image'],
+                transform: function (attributes) {
+                    console.log("attributes of transform:");
+                    console.log(attributes);
+                    return createBlock('core/image', {
+                        url: attributes.url,
+                        alt: attributes.alt
+                    });
+                }
+                // transform: ({ attributes }) => {
+                //     console.log("I have transformed");
+                //     console.log(attributes);
+                //     return createBlock('core/image', {
+                //         content: content
+                //         // textAlignment
+                //     })
+                // }
+            }
+        ]
+    },
+
 
     save: ({ attributes }) => {
         const { title, info, url, alt, id, social } = attributes;
